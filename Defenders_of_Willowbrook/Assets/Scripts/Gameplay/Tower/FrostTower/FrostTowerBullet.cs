6b76d9b63b707b956c5aadec550982a4a5955952
quanggiap2004 <nguyenquanggiap642004@gmail.com>
2025-02-13 04:28:39 +0700

    Part of Level 1

---
+++ Defenders_of_Willowbrook/Assets/Scripts/Gameplay/Tower/FrostTower/FrostTowerBullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrostTowerBullet : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private int bulletDamage = 1;
+
+    private Transform target;
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!target) return;
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * bulletSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collided with: " + other.gameObject.name);
+        other.gameObject.GetComponent<EnemyHealth>().TakeDamage(bulletDamage);
+        Destroy(gameObject);
+    }
+}
