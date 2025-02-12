052e1116f060e132960a4996d959d3862a4cc70c
quanggiap2004 <nguyenquanggiap642004@gmail.com>
2025-02-13 04:34:58 +0700

    Revert "Merge branch 'GiapNQ'"
    This reverts commit 60075d8b3abe00e686f6481893cf49269b707859, reversing
    changes made to f64ba4a2821d725a922555a5982e66da94b0396f.

--- Defenders_of_Willowbrook/Assets/Scripts/LevelDesign/Level1Manager.cs
+++ Defenders_of_Willowbrook/Assets/Scripts/LevelDesign/Level1Manager.cs
@@ -1,51 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class Level1Manager : MonoBehaviour
 {
-    public static Level1Manager main;
-    public Transform spawnPoint;
-    public Transform[] path;
-    public int CurrentMoney;
-    [SerializeField] public TextMeshProUGUI moneyText;
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        main = this;
+        
     }
 
-    public void Start()
+    // Update is called once per frame
+    void Update()
     {
-        CurrentMoney = 100;
-        moneyText.text = CurrentMoney.ToString();
-    }
-
-    public void IncreaseMoney(int amount)
-    {
-        CurrentMoney += amount;
-        UpdateMoneyUI();
-    }
-
-    public bool SpendMoney(int amount)
-    {
-        if(amount <= CurrentMoney)
-        {
-            //Buy
-            CurrentMoney -= amount;
-            UpdateMoneyUI();
-            return true;
-        }
-        else
-        {
-            Debug.Log("Not enough money");
-            return false;
-        }
-    }
-
-    private void UpdateMoneyUI()
-    {
-        if (moneyText != null)
-            moneyText.text = CurrentMoney.ToString();
+        
     }
 }
--- Defenders_of_Willowbrook/Assets/Scripts/UI/MenuController.cs
+++ Defenders_of_Willowbrook/Assets/Scripts/UI/MenuController.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void playGame()
+    // Start is called before the first frame update
+    void Start()
     {
-        SceneManager.LoadScene(1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
