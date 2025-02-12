a917bc63867357d14a63a76934ec123604466cfc
Nguyen Hau <hau.dtnh@gmail.com>
2025-02-12 20:32:36 +0700

    feat: change scene script

--- Defenders_of_Willowbrook/Assets/Scripts/UI/MenuController.cs
+++ Defenders_of_Willowbrook/Assets/Scripts/UI/MenuController.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public void SwitchScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
